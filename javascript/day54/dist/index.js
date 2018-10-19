a55734a1e4b911980a1451da7b8e4908f9e528fd
zhoubudian <1037475507@qq.com>
2018-10-19 08:30:11 +0800

    JavaScript

---
+++ javascript/day54/dist/index.js
@@ -0,0 +1,23 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    showName() {
+        console.log(`我是${this.name},今年${this.age}`);
+    }
+}
+let p = new Person("Alice", 20);
+p.showName();
+
+class Coder extends Person {
+    constructor(name, age, compay) {
+        super(name, age);
+        this.compay = compay;
+    }
+    showCoder() {
+        console.log(`我是${this.name},今年${this.age},在${this.compay}公司`);
+    }
+}
+let c = new Coder("Alic", 20, "BAT");
+c.showCoder();
